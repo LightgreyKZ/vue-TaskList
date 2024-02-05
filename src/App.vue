ea9bfae45dd8f119bf8c598e3401373d8e7bc57a
LightgreyKZ <lightgrey.kz@gmail.com>
2024-02-05 23:10:18 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h2>Список1 задач5:</h2>
+        <p>{{ mytasks }}</p>
+        <p>Count: {{ countTasks }}</p>
+        <!-- <Tasks></Tasks> -->
+    </div>
+</template>
+
+<script>
+import Tasks from '../src/components/TasksList.vue'
+
+export default {
+    data() {
+        return {
+            mytasks: [
+                {
+                    id: '1',
+                    title: '1',
+                    description: '1',
+                    completed: '1'
+                },
+                {
+                    id: '2',
+                    title: '2',
+                    description: '2',
+                    completed: '2'
+                },
+                {
+                    id: '3',
+                    title: '3',
+                    description: '3',
+                    completed: '3'
+                }
+            ]
+        }
+    },
+    components: {
+        Tasks
+    },
+    computed: {
+        countTasks() {
+            return this.mytasks.length;
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+    <h1>Hello!</h1>
+    <p>{{ mytasks }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mytasks: [
+                {
+                    id: '0',
+                    title: '1',
+                    description: '1',
+                    completed: '1'
+                },
+                {
+                    id: '2',
+                    title: '2',
+                    description: '2',
+                    completed: '2'
+                },
+                {
+                    id: '3',
+                    title: '3',
+                    description: '3',
+                    completed: '3'
+                }
+            ]
+        }
+    },
+    computed: {
+        countTasks() {
+            return this.mytasks.length;
+        }
+    }
+}
+</script>
